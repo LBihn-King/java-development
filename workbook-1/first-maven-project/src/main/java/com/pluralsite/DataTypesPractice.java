23eb7f1931daf0654d74d623c4da5a4b35b3df2c
L Bihn-King <lbihnking@gmail.com>
2024-04-09 17:32:01 -0400

    testing commit

--- workbook-1/first-maven-project/src/main/java/com/pluralsite/DataTypesPractice.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsite/DataTypesPractice.java
@@ -1,2 +1,23 @@
-package com.pluralsite;public class DataTypesPractice {
+package com.pluralsite;
+
+public class DataTypesPractice {
+    public static void main(String[] args){
+        int vehicleID;
+        String vehicleMake;
+        String vehicleColor;
+        boolean hasTowingPackage;
+        short currentSpeed = 0;
+        String speedMeasurementType = "mi/h";
+                String odometerReading = currentSpeed + speedMeasurementType;
+        float price;
+        char qualityRating;
+        int phoneNumber;
+        int socialSecurityNumber;
+        int zipCode;
+
+            System.out.println(odometerReading);
+
+
+
+    }
 }
--- workbook-1/first-maven-project/src/main/java/com/pluralsite/PracticeOperations.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsite/PracticeOperations.java
@@ -1,2 +1,36 @@
-package com.pluralsite;public class PracticeOperations {
+package com.pluralsite;
+
+public class PracticeOperations{
+    private String string1, string2, string3;
+    private double double1, double2, double3;
+        
+    public PracticeOperations (String string1, String string2, String string3, double double1, double double2, double double3) {
+        this.string1 = string1;
+        this.string2 = string2;
+        this.string3 = string3;
+        this.double1 = double1;
+        this.double2 = double2;
+        this.double3 = double3;
+        calculate();
+    }
+
+    private void calculate() {
+        String jointString = string1 + string2 + string3;
+            System.out.println("Concatenated String: " + jointString);
+        double addResult = (double1 + double2) + double3;
+            System.out.println("Add Result: " + addResult);
+        double subtResult = (double1 - double2) + double3;
+            System.out.println("Subtract Result: " + subtResult);
+        double multResult = (double1 -double2) + double3;
+            System.out.println("Multiply Result: " + multResult);
+        double divResult = (double1 / double2) / double3;
+            System.out.println("Divide Result: " + divResult);
+
+            double divResultPlusNums = (((divResult + 10) +100) +1000);
+                System.out.println("Division Results + 10, 100, 1000: " + divResultPlusNums);
+
+    }
+    public static void main(String[] args) {
+        PracticeOperations practice = new PracticeOperations("string1 ", "string1 ", "string3", 1.1, 2.2, 3.3);
+    }
 }
