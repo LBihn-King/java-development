b960ef39b4f73c0f68e5763c5ef3c74a8732088e
L Bihn-King <lbihnking@gmail.com>
2024-04-09 18:34:12 -0400

    testing

--- workbook-1/first-maven-project/src/main/java/com/pluralsite/PracticeOperations.java
+++ workbook-1/first-maven-project/src/main/java/com/pluralsite/PracticeOperations.java
@@ -1,10 +1,10 @@
 package com.pluralsite;
 
-public class PracticeOperations{
+public class PracticeOperations {
     private String string1, string2, string3;
     private double double1, double2, double3;
-        
-    public PracticeOperations (String string1, String string2, String string3, double double1, double double2, double double3) {
+
+    public PracticeOperations(String string1, String string2, String string3, double double1, double double2, double double3) {
         this.string1 = string1;
         this.string2 = string2;
         this.string3 = string3;
@@ -16,21 +16,22 @@ public class PracticeOperations{
 
     private void calculate() {
         String jointString = string1 + string2 + string3;
-            System.out.println("Concatenated String: " + jointString);
+        System.out.println("Concatenated String: " + jointString);
         double addResult = (double1 + double2) + double3;
-            System.out.println("Add Result: " + addResult);
+        System.out.println("Add Result: " + addResult);
         double subtResult = (double1 - double2) + double3;
-            System.out.println("Subtract Result: " + subtResult);
-        double multResult = (double1 -double2) + double3;
-            System.out.println("Multiply Result: " + multResult);
+        System.out.println("Subtract Result: " + subtResult);
+        double multResult = (double1 * double2) + double3;
+        System.out.println("Multiply Result: " + multResult);
         double divResult = (double1 / double2) / double3;
-            System.out.println("Divide Result: " + divResult);
+        System.out.println("Divide Result: " + divResult);
 
-            double divResultPlusNums = (((divResult + 10) +100) +1000);
-                System.out.println("Division Results + 10, 100, 1000: " + divResultPlusNums);
+        double divResultPlusNums = (((divResult + 10) + 100) + 1000);
+        System.out.println("Division Results + 10, 100, 1000: " + divResultPlusNums);
 
     }
+
     public static void main(String[] args) {
-        PracticeOperations practice = new PracticeOperations("string1 ", "string1 ", "string3", 1.1, 2.2, 3.3);
+        PracticeOperations practice = new PracticeOperations("string1 ", "string2 ", "string3", 1.1, 2.2, 3.3);
     }
 }
