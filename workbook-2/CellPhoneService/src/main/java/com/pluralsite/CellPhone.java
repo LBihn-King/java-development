4254da8b9cf0b1ce5e14a0fd8ce045e0b52ee458
L Bihn-King <lbihnking@gmail.com>
2024-04-17 14:23:22 -0400

    completed CellPhone exercise

--- workbook-2/CellPhoneService/src/main/java/com/pluralsite/CellPhone.java
+++ workbook-2/CellPhoneService/src/main/java/com/pluralsite/CellPhone.java
@@ -1,6 +1,7 @@
 package com.pluralsite;
-
+import java.util.Scanner;
 public class CellPhone {
+    Scanner scanner = new Scanner(System.in);
     private int serialNumber; //creates the variable so they can be used by the object
     private String model;
     private String carrier;
@@ -15,44 +16,41 @@ public class CellPhone {
         this.owner = owner;
     }
 
+    //getters
     public int getSerialNumber () { //creates a method that can be called to return a variable's value
         return serialNumber; //determines [serial number] as the variable whose value is returned
     }
-
     public String getModel () {
         return model;
     }
-
     public String getCarrier () {
         return carrier;
     }
-
     public String getPhoneNumber () {
         return phoneNumber;
     }
-
     public String getOwner () {
         return owner;
     }
 
+    //setters
     public void setSerialNumber ( int serialNumber){
         this.serialNumber = serialNumber;
     }
-
     public void setModel (String model){
         this.model = model;
     }
-
     public void setPhoneNumber (String phoneNumber){
         this.phoneNumber = phoneNumber;
     }
-
     public void setCarrier (String carrier){
         this.carrier = carrier;
     }
-
     public void setOwner (String owner){
         this.owner = owner;
     }
 
+    public void dial (String dialing) {
+        System.out.println(getOwner() + "'s phone is calling "+ dialing);
+    }
 }
--- workbook-2/CellPhoneService/src/main/java/com/pluralsite/CellPhoneApplication.java
+++ workbook-2/CellPhoneService/src/main/java/com/pluralsite/CellPhoneApplication.java
@@ -6,16 +6,21 @@ public class CellPhoneApplication {
         System.out.println("What is the serial number?");
         phone.setSerialNumber(scanner.nextInt()); //sets value of CellPhone object [phone]'s 'serialNumber' variable to the next int entered
         scanner.nextLine();
+
         System.out.println("What model is the phone?");
         phone.setModel(scanner.nextLine()); //sets value of CellPhone object [phone]'s 'model' variable to the next String entered
+
         System.out.println("Who is the carrier?");
         phone.setCarrier(scanner.nextLine()); //sets value of CellPhone object [phone]'s 'carrier' variable to the next String entered
+
         System.out.println("What is the phone number?");
         phone.setPhoneNumber(scanner.nextLine()); //sets value of CellPhone object [phone]'s 'phoneNumber' variable to the next String entered
+
         System.out.println("Who is the owner of the phone?");
         phone.setOwner(scanner.nextLine()); //sets value of CellPhone object [phone]'s 'owner' variable to the next String entered
     }
-    public static void printCellPhone(CellPhone phone) { //prints values of object
+
+    public static void printCellPhone(CellPhone phone) { //prints values of Cellphone object
         System.out.println(phone.getSerialNumber());
         System.out.println(phone.getModel());
         System.out.println(phone.getCarrier());
@@ -23,10 +28,19 @@ public class CellPhoneApplication {
         System.out.println(phone.getOwner());
     }
 
-    public static void main(String[] args) {
-        CellPhone phone1 = new CellPhone();
-        createCellPhone(phone1);
-        printCellPhone(phone1);
-    }
+    public static void main(String[] args) { //calls methods and runs program
+        CellPhone phone1 = new CellPhone(); //creates CellPhone object
+        CellPhone phone2 = new CellPhone();
 
+        createCellPhone(phone1); //sets phone1's variable values
+        printCellPhone(phone1); //prints phone1's variable values
+        phone1.dial("855-555-2222"); //has phone1 call dial function with manually inputted phone number
+
+        createCellPhone(phone2);
+        printCellPhone(phone2);
+        phone2.dial("123-456-7890");
+
+        phone1.dial(phone2.getPhoneNumber()); //has phone1 call dial with phone2's phone number value
+        phone2.dial(phone1.getPhoneNumber());
+    }
 }
