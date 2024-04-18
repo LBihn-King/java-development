788625687a3bc3e7854fb4c97b26e9155fd99704
L Bihn-King <lbihnking@gmail.com>
2024-04-18 12:10:59 -0400

    completed looping excercise 1

---
+++ workbook-2/LoopingExcercises/src/main/java/com/pluralsite/DoWhile.java
@@ -0,0 +1,11 @@
+package com.pluralsite;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int count = 0;
+        do{
+            System.out.println("I love Java");
+            count++;
+        } while (count < 5);
+    }
+}
---
+++ workbook-2/LoopingExcercises/src/main/java/com/pluralsite/ForLoop.java
@@ -0,0 +1,17 @@
+package com.pluralsite;
+
+public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+        int count;
+
+        for (count = 10; count >= 1; count--) {
+            System.out.println(count);
+            Thread.sleep(1000);
+            if (count == 1) {
+                System.out.println("Launch");
+            }
+
+        }
+
+    }
+}
