17025dd40c07416d6039bfef3dbeb44c62d3e041
L Bihn-King <lbihnking@gmail.com>
2024-04-22 13:01:30 -0400

    finished workbook-3 Exercise 1

---
+++ workbook-3/src/main/java/com/java/FamousQuotes.java
@@ -0,0 +1,52 @@
+package com.java;
+import java.util.Scanner;
+public class FamousQuotes {
+    static Scanner scanner = new Scanner(System.in);
+    static String[] quotes = {"quote1", "quote2", "quote3", "quote4", "quote5", "quote6", "quote7", "quote8", "quote9", "quote10"};
+
+    public static void main(String[] args) {
+        home();
+    }
+
+    public static void home() {
+        System.out.println("1) Choose quote\n2) Random quote\n3)Exit");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            quoteSelect();
+        } else if (choice == 2) {
+            randomQuote();
+        } else {
+            System.out.println("Goodbye");
+            System.exit(0);
+        }
+    }
+
+    public static void quoteSelect() {
+        try {
+            System.out.println("pick quote (#1-#10): ");
+            int index = scanner.nextInt();
+            index--;
+            System.out.println(quotes[index]);
+            anotherQuote();
+        } catch (Exception e) {
+            System.out.println("Number out of range");
+            quoteSelect();
+        }
+    }
+
+    public static void anotherQuote() {
+        System.out.println("Would you like another?\n1) Yes\2) No");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            home();
+        } else {
+            System.out.println("Goodbye");
+            System.exit(0);
+        }
+    }
+    public static void randomQuote() {
+        int index = (int)(Math.random() * 11);
+        System.out.println(quotes[index]);
+        anotherQuote();
+    }
+}
